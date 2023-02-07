d45484c381764e250bb1f94e5fadf2a03aad07d2
Joel Heusala <joelheusala@Joels-MacBook-Pro-2.local>
2023-02-08 01:33:05 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -89,7 +89,7 @@ function createApp(database) {
     return date.getDay() === 1;
   }
 
-  function isHoliday(date) {
+  function isHoliday(date, date2) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = Temporal.PlainDate.from(row.holiday);
