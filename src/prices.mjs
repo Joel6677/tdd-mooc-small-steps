b53172cd9b8f0e94ba6959412f5f384354b81e9d
Joel Heusala <joelheusala@Joels-MacBook-Pro-2.local>
2023-02-08 01:28:42 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -98,8 +98,7 @@ function createApp(database) {
       if (
         date2 &&
         date2.equals(holiday2) &&
-        date.getFullYear() === holiday.getFullYear() &&
-        date.getMonth() === holiday.getMonth() 
+        date.getFullYear() === holiday.getFullYear() 
       ) {
         return true;
       }
