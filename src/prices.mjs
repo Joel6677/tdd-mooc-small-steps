9680bc1ee212bbff0e04452e172624e5cf0b672c
Joel Heusala <joelheusala@Joels-MacBook-Pro-2.local>
2023-02-08 18:29:16 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -38,7 +38,7 @@ function createApp(database) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
-      return calculateCostForDayTicket(age, date, baseCost);
+      return calculateCostForDayTicket(age, date, date2, baseCost);
     }
   }
 
@@ -55,7 +55,7 @@ function createApp(database) {
     return baseCost;
   }
 
-  function calculateCostForDayTicket(age, date, baseCost) {
+  function calculateCostForDayTicket(age, date, date2, baseCost) {
     let reduction = calculateReduction(date);
     if (age === undefined) {
       return Math.ceil(baseCost * (1 - reduction / 100));
