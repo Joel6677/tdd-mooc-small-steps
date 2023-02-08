e77f5c63e8917fa9781b644a85091a709de1d0f0
Joel Heusala <joelheusala@Joels-MacBook-Pro-2.local>
2023-02-08 19:47:34 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -32,11 +32,11 @@ function createApp(database) {
 
   function parsePlainDate(dateString) {return (dateString ? Temporal.PlainDate.from(dateString) : null)}
 
-  function calculateCost(age, type,date2, baseCost) {
+  function calculateCost(age, type,date, baseCost) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
-      return calculateCostForDayTicket(age, date2, baseCost);
+      return calculateCostForDayTicket(age, date, baseCost);
     }
   }
 
