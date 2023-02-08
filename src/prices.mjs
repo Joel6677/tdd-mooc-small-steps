f666c60b74318bf6a539b47118d08a9030144bcb
Joel Heusala <joelheusala@Joels-MacBook-Pro-2.local>
2023-02-08 17:51:51 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -29,6 +29,8 @@ function createApp(database) {
       return Temporal.PlainDate.from(dateString);
     }
   }
+
+  
   
   function calculateCost(age, type, date, baseCost) {
     if (type === "night") {
