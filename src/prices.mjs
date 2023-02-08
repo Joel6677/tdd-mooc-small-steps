64db5bc7c7feb8554237070eeec741dd92a40e39
Joel Heusala <joelheusala@Joels-MacBook-Pro-2.local>
2023-02-08 18:49:10 +0200

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -90,6 +90,7 @@ function createApp(database) {
       let holiday = new Date(row.holiday);
       let holiday2 = Temporal.PlainDate.from(row.holiday)
       if (
+        date2 &&
         date &&
         date.getFullYear() === holiday.getFullYear() &&
         date.getMonth() === holiday.getMonth() &&
